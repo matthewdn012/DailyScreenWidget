4cd0e57ec39bd6c174971f36af06335f68fe2a92
Matthew Nguyen <matthew.nguyen@aveva.com>
2026-05-24 13:03:19 -0700

    initial configuration and folder structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ClockWidget from "./widgets/ClockWidget"
+
+export default function App() {
+	return (
+		<>
+			<ClockWidget />
+		</>
+	);
+}
---
+++ src/widgets/ClockWidget.tsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react"
+
+export default function ClockWidget() {
+	const [now, setNow] = useState(new Date());
+	return null;
+}
